ca7d9c213195d0b00e22176cba9c86d736740242
NikitHome <mishanikita261202@mail.ru>
2022-12-17 17:37:51 +0300

    Create @echcho_bot.py

---
+++ @echcho_bot.py
@@ -0,0 +1,17 @@
+import telebot
+
+# create bot
+bot = telebot.TeleBot('#')
+
+# handler command /start
+@bot.message_handler(commands = ["start"])
+def start(m, res = False):
+	bot.send_message(m.chat.id, 'Привет, напиши мне что-нибудь :)')
+
+# user message handler
+@bot.message_handler(content_types = ["text"])
+def handler_text(message):
+	bot.send_message(message.chat.id, message.text)
+
+# start bot
+bot.polling(non_stop = True, interval = 0)
